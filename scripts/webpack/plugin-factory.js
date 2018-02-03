d6770c1123dd6c37c660809519b0a4032cb9a5b5
Andy Dvorak <advorak@google.com>
2018-02-03 00:15:21 -0800

    WIP: Add missing default value

--- scripts/webpack/plugin-factory.js
+++ scripts/webpack/plugin-factory.js
@@ -29,7 +29,7 @@ module.exports = class PluginFactory {
     outputDirRelativePath,
     pathResolver,
     globber,
-  }) {
+  } = {}) {
     return new CssCleanupPlugin({
       outputDirRelativePath,
       pathResolver,
